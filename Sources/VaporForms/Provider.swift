91602a06a203b841809353597a672fb35520878c
bygri <toby.griffin@me.com>
2016-12-07 14:39:54 +1100

    Update to new Provider syntax

--- Sources/VaporForms/Provider.swift
+++ Sources/VaporForms/Provider.swift
@@ -4,11 +4,13 @@ import Leaf
 public final class Provider: Vapor.Provider {
   public init(config: Config) throws {}
 
-  public func beforeRun(_ drop: Droplet) {
+  public func boot(_ drop: Droplet) {
     let stem = (drop.view as? LeafRenderer)?.stem
     let tags: [Tag] = [ErrorsForField(), IfFieldHasErrors(), LabelForField(), LoopErrorsForField(), ValueForField()]
     tags.forEach {
       stem?.register($0)
     }
   }
+
+  public func beforeRun(_ drop: Droplet) {} // Remove when able
 }
