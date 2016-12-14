ad2240cb7c3627d8aec09408f4534e4c7e6a4d18
Toby Griffin <toby.griffin@me.com>
2016-12-14 20:59:53 +1100

    Update to Vapor 1.3

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "VaporForms",
   dependencies: [
-    .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 2),
+    .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
   ]
 )
